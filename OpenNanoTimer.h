ae3da6f8f994a225522918e6ccd8df3f68d59449
Primrose <primrose@noxxprojects.com>
2021-12-15 01:10:24 +0000

    README.md updated with "How To Use" section

---
+++ OpenNanoTimer.h
@@ -0,0 +1,71 @@
+﻿#ifndef OPENNANOTIMER_H
+#define OPENNANOTIMER_H
+
+#include <chrono>
+
+class OpenNanoTimer
+{
+/** Public functions */
+public:
+    OpenNanoTimer() : Seconds(0), Milliseconds(0), Microseconds(0), Nanoseconds(0)
+    {
+        StartTime = std::chrono::high_resolution_clock::now();
+        EndTime = StartTime;
+    }
+
+    virtual void StartTimer()
+    {
+        StartTime = std::chrono::high_resolution_clock::now();
+    }
+
+    virtual long double EndTimer()
+    {
+        if(StartTime == EndTime)
+        {
+            return 0;
+        }
+        
+        EndTime = std::chrono::high_resolution_clock::now();
+        
+        Seconds = std::chrono::duration<double, std::ratio<1, 1>>(EndTime - StartTime).count();
+        Milliseconds = std::chrono::duration<double, std::milli>(EndTime - StartTime).count();
+        Microseconds = std::chrono::duration<double, std::micro>(EndTime - StartTime).count();
+        Nanoseconds = std::chrono::duration<double, std::nano>(EndTime - StartTime).count();
+        
+        return Nanoseconds;
+    }
+    
+    virtual long double GetSeconds()
+    {
+       return Seconds; 
+    }
+
+    virtual long double GetMilliseconds()
+    {
+        return Milliseconds;
+    }
+
+    virtual long double GetMicroseconds()
+    {
+        return Microseconds;
+    }
+
+    virtual long double GetNanoseconds()
+    {
+        return Nanoseconds;
+    }
+
+/** Public variables */
+public:
+    long double Seconds;
+    long double Milliseconds;
+    long double Microseconds;
+    long double Nanoseconds;
+
+/** Protected variables */
+protected:
+    std::chrono::time_point<std::chrono::high_resolution_clock> StartTime;
+    std::chrono::time_point<std::chrono::high_resolution_clock> EndTime;
+};
+
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# OpenNanoTimer
+## OpenNanoTimer, A Simple C++ High Res Timer Using Chrono.
 A simple C++ high resolution timer, using the chrono library.
+
+Ability to easily Start & Stop the timer, then get the output in
+Seconds, Milliseconds, Microseconds, and Nanoseconds.
+
+## How To Use
+~~~~
+/** Include the header. */
+#include "OpenNanoTimer.h"
+
+/** First declare a new timer. */
+OpenNanoTimer Timer;
+
+/** Then start it. */
+Timer.StartTimer();
+
+/**
+ * Do some work...
+ */
+
+/** End the timer. The output can now be accessed
+ *  through two ways.
+ *  For example, the get the amount of seconds we could do:
+ *      Timer.GetSeconds();
+ *  OR
+ *      Timer.Seconds;
+ */
+Timer.EndTimer();
+
+/** Get the amount of seconds, using the function in this case. */
+const long double Seconds = Timer.GetSeconds();
+
+/**
+ * Printing the values.
+ * This section show how to print the values, at varrying lengths.
+ */
+
+/** Using %g, we're that we want the best fit. */
+printf("Seconds: %g\n", Seconds);
+
+/** Using %0.12g, we're saying that we want at least 12 digits
+ *  both before and after the decimal. */
+printf("Seconds: %12.12g\n", Seconds);
+
+/** For further help with printing floats/doubles, see the link below:
+ *  https://www.lemoda.net/c/double-decimal-places */
+~~~~
